13d41818c7151d1b42278cb1ce51b7fdcb9f9a47
Anshuman Jaiswal <anshumanjofficial@gmail.com>
2023-09-28 23:10:07 +0530

    groceries

---
+++ groceries.js
@@ -0,0 +1,8 @@
+let range1 = document.getElementById("range-1");
+let range2 = document.getElementById("range-2");
+let slider1 = document.getElementById("slider-1");
+let slider2 = document.getElementById("slider-2");
+
+let minGap = 1;
+
+  
